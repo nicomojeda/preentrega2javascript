27062d0cb7d8b68256a0cd16890661dda92b4000
Nicolas Ojeda <nicolas-m-ojeda@outlook.com>
2023-09-20 10:34:48 -0300

    pre entrega 3

--- preentrega2.js
+++ preentrega2.js
@@ -2,63 +2,40 @@
 
 
 //OBJETO CORRESPONDIENTE A LA SOLICITUD DE PRESTAMO POR PARTE DE UN CLIENTE
-class cliente_prestamo {
+class cliente {
 
-    constructor(nombre, apellido, dni, prestamo, cuotas){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.prestamo = prestamo;
-        this.cuotas = cuotas
+    constructor(usuario, contrasenia){
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.prestamo;
+        this.cuotas;
         this.dinero_con_intereses;
         this.cuota_precio;
     }
 
     //METODOS OBJETO
 
-    getDatosCliente(){
-        this.getNombre();
-        this.getApellido();
-        this.getDNI();
-        this.getPrestamo();
-        this.getCuotas();
-        this.getDineroConIntereses();
-        this.getCuotasPrecioFinal();
+    setPrestamo(prestamo){
+
+        this.prestamo = prestamo;
+
     }
 
-    getNombre(){
-        console.log("Nombre: ",this.nombre)
-    }
-
-    getApellido(){
-        console.log("Apellido: ",this.apellido)
-    }
-
-    getApellido(){
-        console.log("Apellido: ",this.apellido)
-    }
-
-    getDNI(){
-        console.log("DNI: ",this.dni)
-    }
 
     getPrestamo(){
-        console.log("Su dinero solicitado es: ",this.prestamo)
+        console.log("Su dinero solicitado es: ",this.prestamo);
     }
 
     
     
     getCuotas(){
-        console.log("Su cantidad de cuotas seleccionada es: ",this.cuotas)
+       return this.cuotas;
     }
 
     getDineroConIntereses(){
-        console.log("Este es el dinero que deberá devolver: $",this.dinero_con_intereses)
+        return this.dinero_con_intereses;
     }
 
-    getCuotasPrecioFinal(){
-        console.log("El valor de su cuota es: ", this.cuota_precio)
-    }
 
     cuotaValida(cuotas){
         let flagCuotas = false;
@@ -99,60 +76,163 @@ class cliente_prestamo {
     
     valor_cuota(){
         this.cuota_precio = Math.round(this.prestamo/this.cuotas);
+        return this.cuota_precio;
     }
     
 
 
 }
 
-//Arreglo de clientes
-
-let listaClientes = [];
-let listaNombres = [];
 
 
-function agradecimiento(nombre_cliente){
-    console.log("¡ Gracias por elegirnos",nombre_cliente,"!");
-}
 
-function agradecimiento_clientes(arreglo, agradecimiento){
-     agradecimiento(arreglo[arreglo.length-1]);
+
+function recorreStorage(arregloGuardado){
+    for (let i=0; i<localStorage.length; i++){
+        let clave = localStorage.key(i);
+        arregloGuardado.push(JSON.parse(localStorage.getItem(clave)));
+    }
 }
 
 
 
 
-//MAIN
-let solicitud_prestamo = prompt("¿Desea solicitar un prestamo? SI/NO");
+let usuarios = [];
+recorreStorage(usuarios);
+
+let btnregistrate = document.getElementById("btn_registrar");
+btnregistrate.addEventListener( "click" , registrar );
 
 
-    while( solicitud_prestamo.toUpperCase() == "SI"){
-        let nombre = prompt("Ingrese su nombre:");
-        let apellido = prompt("Ingrese su apellido:");
-        let dni = prompt("Ingrese su DNI:");
-        let dinero_solicitado = prompt("Ingrese la cantidad de dinero que necesita:");
-        let cuotas = parseInt (prompt("Ingrese cantidad de cuotas a cancelar:"));
+let btnLogIn = document.getElementById("btn_logIn");
+btnLogIn.addEventListener( "click" , loguearse );
 
-        let cliente = new cliente_prestamo(nombre, apellido, dni, dinero_solicitado, cuotas);
-        cliente.cuotaValida(cuotas);
-        cliente.dinero_intereses();
-        cliente.valor_cuota();
+let formularioRegistro = document.getElementById("formulario");
+formularioRegistro.addEventListener("submit", function(e){e.preventDefault();})
 
 
-        cliente.getDatosCliente();
+function registrar() {
 
-        //Inclusion en arreglo
+    let nombreUsuario = document.getElementById("userName").value;
+    let contrasenia = document.getElementById("password").value;
+    let contador = 0;
 
-        listaClientes.push(cliente);
-        listaNombres.push(nombre);
+    for( let i = 0 ; i<usuarios.length; i++){
 
-        agradecimiento_clientes(listaNombres, agradecimiento);
+        if(usuarios[i].usuario == nombreUsuario){
+            contador++;
 
+        }
+    }
 
-        solicitud_prestamo = prompt("¿Desea solicitar otro prestamo? SI/NO");
+        if (contador == 0){
+
+            let nuevoCliente = new cliente(nombreUsuario, contrasenia);
+
+            let clienteJson = JSON.stringify( nuevoCliente );
+
+            localStorage.setItem( Math.random() , clienteJson);
+
+            document.body.innerHTML = `<header><h1>¡Registro exitoso!</h2></header>
+                                        <h2>¡El usuario: ${nombreUsuario} se ha registrado con exito!</h2> <a href="index.html">Volver</a>
+                                        `;
+        }
+        else{
+
+            document.body.innerHTML = `<header><h1>¡Error!</h1></header>
+                                        <h2>¡El usuario: ${nombreUsuario} ya fue registrado!</h2> <a href="index.html">Volver</a>`;
+            console.log("el usuario ya ha sido registrado");
+
         }
 
-console.log("La simulación del prestamo ha finalizado")
+}
+
+function loguearse(){
+
+    let nombreUsuario = document.getElementById("userName").value;
+    let contrasenia = document.getElementById("password").value;
+    let contador = 0 ;
+
+    for( let i = 0; i<usuarios.length ; i++){
+
+        if( usuarios[i].usuario == nombreUsuario && usuarios[i].contrasenia == contrasenia){
+
+            contador++;
+
+        }
+    }
+
+    if(contador == 1){
+            document.body.innerHTML = `<header><h1>Usted esta dentro del simulador de prestamo</h2></header>
+            <main>
+            <h2>¡Bievenido ${nombreUsuario} !</h2> 
+            <form action="" id="formulario2">
+                </li>
+                <li>
+                    <label for="">Dinero solicitado</label>
+                    <input type="text" id="dineroSolicitado">
+                </li>
+                <li>
+                    <label for="">Cantidad de cuotas</label>
+                    <input type="text" id="cuotasSolicitadas">
+                </li>
+                <li>
+                    <button id = "btn_solicitudDinero" >Solicitar</button>
+                </li>
+            </form>
+            </main>`;
+
+            let formularioSolicitud = document.getElementById("formulario2");
+            formularioSolicitud.addEventListener("submit", function(e){e.preventDefault();})
+
+            
+            let btnSolicitarDinero = document.getElementById("btn_solicitudDinero");
+            btnSolicitarDinero.addEventListener( "click" , solicitud );
+
+
+            function solicitud (){
+
+
+                let clienteEsporadico = new cliente(nombreUsuario, contrasenia);
+
+
+                let cuotaNueva = document.getElementById("cuotasSolicitadas").value;
+                let prestamoNuevo = document.getElementById("dineroSolicitado").value;
+
+                clienteEsporadico.setPrestamo(prestamoNuevo);
+                clienteEsporadico.cuotaValida(cuotaNueva);
+                clienteEsporadico.dinero_intereses();
+                let valorFinalCuota = clienteEsporadico.valor_cuota();
+                let valorFinalDevolucion = clienteEsporadico.getDineroConIntereses();
+
+                document.body.innerHTML = `<header><h1>Solicitud de prestamo realizada</h1></header>
+                                            <main>
+                                                <h2>Su prestamo solicitado fue:</h2>
+                                                <section>
+                                                    <li>Dinero solicitado: ${prestamoNuevo}</li>
+                                                    <li>Cantidad de cuotas seleccionadas: ${clienteEsporadico.getCuotas()}</li>
+                                                    <li>El dinero que deverá devolver es: ${valorFinalDevolucion}</li>
+                                                    <li>El valor de la cuota es: ${valorFinalCuota}</li>
+                                                    <li><a href="index.html">Volver al inicio</a>
+                                                </section>
+                                                <div><h3>La simulación de prestamo ha finalizado</h3>
+                                            </main>`;
+
+
+
+
+            }
+
+
+
+        }
+
+        else{
+
+            document.main.innerHTML = `<h2>Usted no esta registrado. Por favor registrese y vuelva a intentarlo.</h2> <a href="index.html">Volver</a>`;
+
+        }
+}
 
 
 
