fb18e8fc5bd6346d104c9ae1255fc7ce8685fee2
Nicolas Ojeda <nicolas-m-ojeda@outlook.com>
2023-10-07 20:43:21 -0300

    Entrega proyecto Final javaScript

--- preentrega2.js
+++ preentrega2.js
@@ -18,9 +18,11 @@ class cliente {
     setPrestamo(prestamo){
 
         this.prestamo = prestamo;
-
     }
 
+    setCuota(cuota){
+        this.cuotas=cuota;
+    }
 
     getPrestamo(){
         console.log("Su dinero solicitado es: ",this.prestamo);
@@ -37,21 +39,22 @@ class cliente {
     }
 
 
-    cuotaValida(cuotas){
-        let flagCuotas = false;
+  /*  cuotaValida(cuotas){
+        //let flagCuotas = false;
 
-        while(flagCuotas == false){
+        //while(flagCuotas == false){
 
             if(cuotas == 1 || cuotas == 3 || cuotas == 6 || cuotas == 12 || cuotas == 18 || cuotas == 24){
                 this.cuotas = cuotas;
                 flagCuotas = true;
             }
             else{
-                cuotas =prompt("Ingrese una cantidad de cuotas valida: ")
+                //cuotas =prompt("Ingrese una cantidad de cuotas valida: ")
+                Toastify({text: "Ingrese una cantidad de cuotas valida", duration: "5000", backgroundColor: "red"}).showToast();  
             }
-        }
+        //}
 
-    }
+    }*/
 
     dinero_intereses(){
         if(this.cuotas == 1){
@@ -85,7 +88,7 @@ class cliente {
 
 
 
-
+//INICIO DE PAGINA
 
 function recorreStorage(arregloGuardado){
     for (let i=0; i<localStorage.length; i++){
@@ -103,6 +106,8 @@ recorreStorage(usuarios);
 let btnregistrate = document.getElementById("btn_registrar");
 btnregistrate.addEventListener( "click" , registrar );
 
+let main = document.getElementById("pagPrincipal");
+
 
 let btnLogIn = document.getElementById("btn_logIn");
 btnLogIn.addEventListener( "click" , loguearse );
@@ -111,70 +116,73 @@ let formularioRegistro = document.getElementById("formulario");
 formularioRegistro.addEventListener("submit", function(e){e.preventDefault();})
 
 
+
+//FUNCION DE REGISTRO
 function registrar() {
 
     let nombreUsuario = document.getElementById("userName").value;
     let contrasenia = document.getElementById("password").value;
-    let contador = 0;
 
-    for( let i = 0 ; i<usuarios.length; i++){
+    //USO DE FIND
+    let buscarUsuario = usuarios.find( (users) => users.usuario == nombreUsuario);
 
-        if(usuarios[i].usuario == nombreUsuario){
-            contador++;
-
-        }
+    if ( buscarUsuario != undefined){
+        Toastify({text: "¡El usuario ingresado ya fue registrado previamente!", duration: "5000"}).showToast();   
     }
-
-        if (contador == 0){
-
+    else{ 
             let nuevoCliente = new cliente(nombreUsuario, contrasenia);
 
             let clienteJson = JSON.stringify( nuevoCliente );
 
             localStorage.setItem( Math.random() , clienteJson);
 
-            document.body.innerHTML = `<header><h1>¡Registro exitoso!</h2></header>
-                                        <h2>¡El usuario: ${nombreUsuario} se ha registrado con exito!</h2> <a href="index.html">Volver</a>
-                                        `;
+            usuarios.push(nuevoCliente);
+
+            Toastify({text: "¡Registro exitoso!", duration: "5000",}).showToast(); 
+        
         }
-        else{
-
-            document.body.innerHTML = `<header><h1>¡Error!</h1></header>
-                                        <h2>¡El usuario: ${nombreUsuario} ya fue registrado!</h2> <a href="index.html">Volver</a>`;
-
-        }
-
-}
-
+    }
+                            
+    
+    
+    
+    //FUNCION LOGUEO
+                                        
 function loguearse(){
 
     let nombreUsuario = document.getElementById("userName").value;
     let contrasenia = document.getElementById("password").value;
-    let contador = 0 ;
+    let buscarUsuario = usuarios.find( (users) => users.usuario == nombreUsuario);
 
-    for( let i = 0; i<usuarios.length ; i++){
+    //Uso de find
+    let buscarContrasenia = usuarios.find( (users) => users.contrasenia == contrasenia);
 
-        if( usuarios[i].usuario == nombreUsuario && usuarios[i].contrasenia == contrasenia){
+    //OPCION USUARIO EXISTE
+    if(buscarUsuario == undefined || buscarContrasenia == undefined){
 
-            contador++;
+        //uso de librerias
+        Toastify({text: "Usuario o contraseña inexistente", duration: "5000"}).showToast();
 
-        }
     }
-
-    if(contador == 1){
+    else{
             document.body.innerHTML = `<header><h1>Usted esta dentro del simulador de prestamo</h2></header>
-            <main>            
-            <section>
-                <h2>¡Bievenido ${nombreUsuario} !</h2>
+            <main class="conteiner row justify-content-md-center">            
+            <section class="seccionPrincipal row column-3">
+                <h2 class="class="row column-3">¡ Bievenido ${nombreUsuario} !</h2>
                 <form action="" id="formulario2">
                     </li>
                     <li>
-                        <label for="">Dinero solicitado</label>
-                        <input type="text" id="dineroSolicitado">
+                        <label for="" class="row column-3 ds">Dinero solicitado</label>
+                        <input type="text" id="dineroSolicitado" class="row column-3 ds">
                     </li>
                     <li>
-                        <label for="">Cantidad de cuotas</label>
-                        <input type="text" id="cuotasSolicitadas">
+                        <select class="form-select row column-3" aria-label="Default select example" id="cuotasSolicitadas">
+                        <option value="3">3</option>
+                        <option value="6">6</option>
+                        <option value="12">12</option>
+                        <option value="18">18</option>
+                        <option value="24">24</option>
+                        </select>
                     </li>
                     <li>
                         <button id = "btn_solicitudDinero" >Solicitar</button>
@@ -190,7 +198,7 @@ function loguearse(){
             let btnSolicitarDinero = document.getElementById("btn_solicitudDinero");
             btnSolicitarDinero.addEventListener( "click" , solicitud );
 
-
+//SOLICITA PRESTAMO
             function solicitud (){
 
 
@@ -201,7 +209,7 @@ function loguearse(){
                 let prestamoNuevo = document.getElementById("dineroSolicitado").value;
 
                 clienteEsporadico.setPrestamo(prestamoNuevo);
-                clienteEsporadico.cuotaValida(cuotaNueva);
+                clienteEsporadico.setCuota(cuotaNueva);
                 clienteEsporadico.dinero_intereses();
                 let valorFinalCuota = clienteEsporadico.valor_cuota();
                 let valorFinalDevolucion = clienteEsporadico.getDineroConIntereses();
@@ -210,10 +218,10 @@ function loguearse(){
                                             <main>
                                                 <section>
                                                     <h2>Su prestamo solicitado fue:</h2>
-                                                        <li>Dinero solicitado: ${prestamoNuevo}</li>
+                                                        <li>Dinero solicitado: $${prestamoNuevo}</li>
                                                         <li>Cantidad de cuotas seleccionadas: ${clienteEsporadico.getCuotas()}</li>
-                                                        <li>El dinero que deverá devolver es: ${valorFinalDevolucion}</li>
-                                                        <li>El valor de la cuota es: ${valorFinalCuota}</li>
+                                                        <li>El dinero que deverá devolver es: $${valorFinalDevolucion}</li>
+                                                        <li>El valor de la cuota es: $${valorFinalCuota}</li>
                                                         <li><a href="index.html">Volver al inicio</a>
                                                     <div><h3>La simulación de prestamo ha finalizado</h3>
                                                 </section>
@@ -228,12 +236,42 @@ function loguearse(){
 
         }
 
-        else{
-
-            document.main.innerHTML = `<h2>Usted no esta registrado. Por favor registrese y vuelva a intentarlo.</h2> <a href="index.html">Volver</a>`;
-
-        }
 }
 
+//FOOTER FETCH
 
+let dolarOficialCompra = document.getElementById("dolarOficialCompra");
+let dolarOficialVenta = document.getElementById("dolarOficialVenta");
+let dolarBlueCompra = document.getElementById("dolarBlueCompra");
+let dolarBlueVenta = document.getElementById("dolarBlueVenta");
+
+fetch("https://api.bluelytics.com.ar/v2/latest")
+    .then( response => response.json())
+    .then( data => dolarOficialCompra.innerHTML =`<h3>DOLAR OFICIAL COMPRA</h3>
+                                                <img src="./img/dolar-cambio.png" alt="bolsa dinero">
+                                                <h4>$ ${data.oficial.value_buy}</h4>`)
+
+fetch("https://api.bluelytics.com.ar/v2/latest")
+        .then( response => response.json())
+        .then( data2 => dolarOficialVenta.innerHTML =`<h3>DOLAR OFICIAL VENTA</h3>
+                                                    <img src="./img/dolar-cambio.png" alt="bolsa dinero">
+                                                    <h4>$ ${data2.oficial.value_sell}</h4>`)
+
+fetch("https://api.bluelytics.com.ar/v2/latest")
+        .then( response => response.json())
+        .then( data3 => dolarBlueCompra.innerHTML =`<h3>DOLAR BLUE COMPRA</h3>
+                                                    <img src="./img/dolar-cambio.png" alt="bolsa dinero">
+                                                    <h4>$ ${data3.blue.value_buy}</h4>`)
+
+fetch("https://api.bluelytics.com.ar/v2/latest")
+        .then( response => response.json())
+        .then( data4 => dolarBlueVenta.innerHTML =`<h3>DOLAR BLUE VENTA</h3>
+                                                    <img src="./img/dolar-cambio.png" alt="bolsa dinero">
+                                                    <h4>$ ${data4.blue.value_sell}</h4>`)
+
+
+
+
+
+ 
 
